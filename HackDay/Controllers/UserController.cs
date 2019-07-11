d5c38ed941b4f32a26a23282cddfc9345af5327b
Dipakkumar Rohit <dipakkumar.rohit@numerator.com>
2019-07-11 18:13:52 +0530

    no message

---
+++ HackDay/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HackDay.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+        public ActionResult Archive()
+        {
+            return View();
+        }
+        public ActionResult PendingRequest()
+        {
+            return View();
+        }
+
+    }
+}
